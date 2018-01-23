3310a9a09377b825502072419a5d57d482f2baf8
NikSavilov <niksavilov@gmail.com>
2018-01-24 02:10:59 +0300

    First dialogue. Working fine!

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
-import telebot
+import telebot, sys, random
 import requests
+from package import vk
 from bs4 import BeautifulSoup
-
-access_token = '335894434:BAF8cRoHH8syXSBzhJqeYrrwnaiBKDTnefY'
-bot = telebot.TeleBot(access_token)
 
 def get_page(group = 'M3100', week = ''):
     if week:	
@@ -40,10 +38,51 @@ def send_schedule(message):
 
     bot.send_message(message.chat.id, resp, parse_mode='HTML')
 
-@bot.message_handler(content_types=["text"])
-def define_request_type(message):
-	words = message.text.split()
-	
+def find_longest_word(message):
+	longest_word = 'lol'
+	max_len = 0
+	for word in message.split():
+		if len(word) > max_len:
+			max_len = len(word)
+			longest_word = word
+	return longest_word
 
+#### Getting messages from some chat and creating a bot #####
+access_token = '335894434:AAF8cRoHH8syXSBzhJqeYrrwnaiBKDTnefY'
+bot = telebot.TeleBot(access_token)
+profile = 175747053	# mal 175747053 # ler 101909873
+messages = vk.messages_aggregator(profile)
+
+##############################################################
+
+@bot.message_handler(content_types=["text"])
+def define_request_type(message,messages = messages):
+	try:
+		message.text = message.text.lower() 
+		answers = [{'num': num, 'text': mes['body'].lower(), 'from_id': mes['from_id'] } for num, mes in enumerate(messages)]
+		founded_requests = []
+		founded_answers = []
+		possible_answers = []
+		key_word = find_longest_word(message.text)
+		for req in answers:
+			chat_key_word = find_longest_word(req['text'])
+			if chat_key_word in message.text.split():
+				founded_requests.append(req)
+			elif message.text == req['text']:
+				founded_requests.append(req)
+				#print("req",req)
+		for req in founded_requests:
+			num = req['num']
+			from_id = req['from_id']
+			#print('found', req)
+			for i in range(num -1,-1,-1):
+				#print(i, answers[i])
+				if answers[i]['from_id'] != from_id:
+					founded_answers.append(answers[i]['text'])
+					break
+		bot.send_message(message.chat.id, founded_answers[random.randint(0, len(founded_answers) - 1)])			
+	except:
+		unknow_answers = ['Черт. Я потекла', 'Слава солнцеликому!!1!1!', 'не пойму тебя', 'хмммм', 'рот закрой!!)','хочу на мооооре', 'фух']
+		bot.send_message(message.chat.id, unknow_answers[random.randint(0, 6)])	
 if __name__ == '__main__':
     bot.polling(none_stop=True)
