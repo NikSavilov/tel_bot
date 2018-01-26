67336ecffda8b74986b676065d3cdea5a43551dc
NikSavilov <niksavilov@gmail.com>
2018-01-26 03:01:16 +0300

    Text definer v1, problems with normalized words

--- package/metro.py
+++ package/metro.py
@@ -1,6 +1,11 @@
 import requests, sys, selenium, time, os
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from PIL import Image
+from selenium.webdriver.common.by import By
+import mysql.connector
+from rutermextract import TermExtractor
+import pymorphy2
 
 def get_screened_page(from_station, to_station):
 	url_params = {
@@ -22,10 +27,68 @@ def get_screened_page(from_station, to_station):
 		print("Request wasn't successful")
 		sys.exit(0)
 	screenshot = Image.open(name)
-	screenshot = screenshot.crop((0,100,245,700))
+	screenshot = screenshot.crop((10,80,310,805))
 	screenshot.save(name)
 	pass
 
+def stations_parser():
+	cnx = mysql.connector.connect(
+		user='ih813395_bot', 
+		password='123123123bot',
+		host='185.125.219.232',
+		database='ih813395_bot_metro')
+	cursor = cnx.cursor()
+	DRIVER = 'chromedriver'
+	driver = webdriver.Chrome(DRIVER)
+	for i in range(67,68):
+		url = 'https://metro.yandex.ru/spb?from={i}&to=1&route=0'.format(i = i)
+		driver.get(url)
+		time.sleep(2)
+		metro_name = driver.find_element(By.CLASS_NAME, "route-details-block__terminal-station").text
+		print(metro_name)
+		term_extractor = TermExtractor()
+		metro_normalized = ""
+		for term in term_extractor(metro_name, strings=True):
+			metro_normalized = term
+		if len(metro_normalized) < 2:
+			metro_normalized = metro_name.lower()
+		print(metro_normalized)
+		request = "insert into stations(number, name) values ({number},{name})".format(number = i, name = '"' + metro_normalized + '"')
+		#print(request)
+		cursor.execute(request)
+		cnx.commit()
+	pass
 
-get_screened_page(67,6)
+def text_request_definer(message):
+	bugs_list = [
+	'маршрут', 
+	'метро', 
+	'станция', 
+	'проложи', 
+	'на'
+	]
+	message = message.split()
+	text = [word for word in message if word not in bugs_list]
+	from_station = []
+	to_station = []
+	for word in text:
+		if word == 'от':
+			flag_from = True
+			flag_to = False
+		elif word == 'до':
+			flag_to = True
+			flag_from = False
+		else:
+			if flag_from:
+				from_station.append(word)
+			elif flag_to:
+				to_station.append(word)
+	morph = pymorphy2.MorphAnalyzer()
+	from_normalized = [morph.parse(word)[0].normal_form for word in from_station]
+	to_normalized = [morph.parse(word)[0].normal_form for word in to_station]
+	
+	return from_normalized, to_normalized
 
+# get_screened_page(67,6)
+#stations_parser()
+print(text_request_definer('проложи маршрут на метро от академической до технологического института'))
---
+++ vk.py
@@ -0,0 +1,145 @@
+import requests, random, time, traceback, itertools, math, sys
+import datetime
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import pytz
+plotly.tools.set_credentials_file(username='niksavilov', api_key='Qxc4TAOOwk8CU1os7wNx')
+global access_token
+global tz
+tz = ''
+access_token = '28e237e0c7934f94028c0e66ed9ffda21e2d49388f57070549425cdcfb909770dd755e971d383b97cbb76'
+def get(url, params={}, timeout=5, max_retries=5, backoff_factor=1.3):
+    """ Выполнить GET-запрос
+
+    :param url: адрес, на который необходимо выполнить запрос
+    :param params: параметры запроса
+    :param timeout: максимальное время ожидания ответа от сервера
+    :param max_retries: максимальное число повторных запросов
+    :param backoff_factor: коэффициент экспоненциального нарастания задержки
+    """
+    delay = 1
+    retries = 0
+    while retries < max_retries:
+        try:
+            retries+=1
+            response = requests.get(url, timeout = timeout)
+            if response.ok:
+                break
+            else:
+                print("delay ", delay)
+                time.sleep(delay)
+                delay = backoff_factor * delay + random.gauss(delay * 0.1, 1)
+        except requests.exceptions.RequestException:
+            print("Ooops. Err {err} occured. Sleep: {delay}".format(delay = delay, err = traceback.format_exception_only()))
+            time.sleep(delay)
+            delay = backoff_factor * delay + random.gauss(delay * 0.8, 0.8)
+    if response.ok:
+    	print('response was accepted')
+    	return response
+    else:
+    	print('response wasnt accepted')
+    	return('error') 
+def get_friends(user_id, fields):
+    """ Returns a list of user IDs or detailed information about a user's friends """
+    assert isinstance(user_id, int), "user_id must be positive integer"    
+    assert isinstance(fields, str), "fields must be string"
+    assert user_id > 0, "user_id must be positive integer"
+    domain = "https://api.vk.com/method"
+    # access_token = 'ffb882a0affcbaadecfc56f7cbeeac328fded8f4c01f59d2981c46643fa9e1151ed2f865c11fb1fe64566'
+    # user_id = 141839173
+
+    query_params = {
+    'domain' : domain,
+    'access_token': access_token,
+    'user_id': user_id,
+    'fields': fields
+    }
+
+    query = "{domain}/friends.get?access_token={access_token}&user_id={user_id}&fields={fields}&v=5.53".format(**query_params)
+    response = get(query)
+    try:
+        return response.json()['response']
+    except:
+        print("Invalid user id or access_token")
+        sys.exit(1)
+def age_predict(user_id):
+    assert isinstance(user_id, int), "user_id must be positive integer"
+    assert user_id > 0, "user_id must be positive integer"
+    summa = datetime.datetime.now() - datetime.datetime.now()
+    count = 0
+    for item in get_friends(user_id,'bdate')['items']:
+    	s = item.get('bdate', '0')
+    	if len(s) >= 8:
+            print(s,item.get('first_name'),item.get('last_name'))
+            age = datetime.datetime.now() - datetime.datetime.strptime(s,"%d.%m.%Y")
+            summa += age
+            count += 1
+    return round(int(summa.days)/(365*count))
+def messages_get_history(user_id, offset=0, count=200):
+    assert isinstance(user_id, int), "user_id must be positive integer"
+    assert user_id > 0, "user_id must be positive integer"
+    assert isinstance(offset, int), "offset must be positive integer"
+    assert offset >= 0, "offset must be positive integer"
+    assert count >= 0, "count must be positive integer"
+    domain = "https://api.vk.com/method"
+    # user_id = 141839173
+
+    query_params = {
+    'domain' : domain,
+    'access_token': access_token,
+    'user_id': user_id,
+    'count' : count,
+    'offset' : offset
+    }
+
+    query = "{domain}/messages.getHistory?access_token={access_token}&user_id={user_id}&offset={offset}&count={count}&v=5.53".format(**query_params)
+    response = get(query)
+    try:
+        return response.json()['response']
+    except:
+        print("Invalid user id or access_token")
+        exit(1)
+def messages_aggregator(user_id):
+    offset = 0
+    messages = []
+    while True:
+        response = messages_get_history(user_id = user_id, offset = offset)
+        if response['items']:
+            for g in response['items']:
+                messages.append(g)
+        else:
+            return messages
+        offset += 200
+        time.sleep(0.4)
+def count_dates_from_messages(messages):
+    dates_arr = []
+    for k in messages:
+        date = datetime.datetime.fromtimestamp(k['date']).strftime("%Y.%m.%d")
+        dates_arr.append(date)
+    dates_list = list(set(dates_arr))
+    frequency = []
+    for j in dates_list:
+        frequency.append({'date' : j, 'messages' : dates_arr.count(j)})
+        # print({'date' : j, 'messages' : dates_arr.count(j)})
+    frequency_sorted = sorted(frequency, key = lambda x: x['date'])
+    return frequency_sorted
+def plot_maker(dates_list):
+    x = []
+    y = []
+    for j in dates_list:
+        x.append(datetime.datetime.strptime(j.get('date'),"%Y.%m.%d").replace(tzinfo= pytz.utc))
+        y.append(j.get('messages'))
+    data = [go.Scatter(x = x, y = y)]
+    py.plot(data)
+    pass
+    
+def get_network(users_ids, as_edgelist=True):
+    """ Building a friend graph for an arbitrary list of users """
+    # PUT YOUR CODE HERE
+    pass
+
+# Example requests:
+# plot_maker(count_dates_from_messages(messages_aggregator(162674267)))
+# print('Possible age is', age_predict(141839173))
+# 
